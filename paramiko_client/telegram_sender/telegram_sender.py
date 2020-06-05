dc54cc2b33cc8cff5676f11b202ffea9feb8daad
illmatik12 <gcnation@gmail.com>
2020-06-06 00:04:11 +0900

    add http basic auth

--- paramiko_client/telegram_sender/telegram_sender.py
+++ paramiko_client/telegram_sender/telegram_sender.py
@@ -1,17 +1,16 @@
 import requests
-
-
+from requests.auth import HTTPBasicAuth
 
 class TelegramSender:
     def __init__(self, host, api_path ):
         self.host = host 
         self.api_path = api_path #send api 
-    
+   
     def send_message(self, send_msg):
         url= self.host + self.api_path
         params  = {'send_msg' : send_msg}
         try:
-            response = requests.get(url, params  = params)
+            response = requests.get(url, params = params, auth =HTTPBasicAuth('semi','love') )
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
